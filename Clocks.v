c869f3f0c775dd8cd6fe3033b2d751be385c5a59
Grygoriy Zholtkevych <g.zholtkevych@karazin.ua>
2023-12-21 21:13:58 +0200

    update

--- Clocks.v
+++ Clocks.v
@@ -1,21 +1,56 @@
 Require Export Utf8.
-Require Import Arith.PeanoNat.
+Require Import Init.
 
 
-Inductive clock := clk : nat -> clock.
-Definition id (c : clock) : nat := let 'clk n := c in n.
+Inductive clock : Set :=
+  | some : clock
+  | other : clock -> clock.
 
-Lemma id_clk : ∀ n, id (clk n) = n.
-Proof. trivial. Qed.
+Fixpoint to_nat (c : clock) : nat :=
+  match c with
+  | some => 0
+  | other c' => S (to_nat c')
+  end.
 
-Lemma clk_id : ∀ c, clk (id c) = c.
-Proof. intro. now destruct c as [n]. Qed.
+Fixpoint from_nat (n : nat) : clock :=
+  match n with
+  | 0 => some
+  | S n' => other (from_nat n')
+  end.
+
+Lemma to_from_nat : ∀ n, to_nat (from_nat n) = n.
+Proof.
+  intro.
+  induction n as [| n' IHn'].
+  - trivial.
+  - simpl. now rewrite IHn'.
+Qed.
+
+Lemma from_to_nat : ∀ c, from_nat (to_nat c) = c.
+Proof.
+  intro.
+  induction c as [| c' IHc'].
+  - trivial.
+  - simpl. now rewrite IHc'.
+Qed.
 
 Definition clock_eq_dec : ∀ c1 c2 : clock, {c1 = c2} + {c1 ≠ c2}.
 Proof.
-  intros.
-  destruct c1 as [n1], c2 as [n2].
-  destruct (Nat.eq_dec n1 n2) as [E | NE].
-  - left. now rewrite E.
-  - right. intro. apply NE. now injection H.
+  intro.
+  induction c1 as [| c1' IHc1'].
+  - destruct c2 as [| c2'].
+    + now left.
+    + right. discriminate.
+  - intro.
+    induction c2 as [| c2' IHc2'].
+    + right. discriminate.
+    + destruct (IHc1' c2').
+      * left. now rewrite e.
+      * right. intro. apply n. now injection H. 
 Defined.
+
+
+Definition ClockSet :=
+  {cs : clock → Prop | ∃ ecs : list clock, ∀ c, cs c → In c ecs}.
+Definition toPredicate (cs : ClockSet) : clock → Prop := proj1_sig cs.
+Coercion toPredicate : ClockSet >-> Funclass.
--- EventSpace.v
+++ EventSpace.v
@@ -1,35 +1,7 @@
 Require Export EventSpaces.Clocks.
 
 
-Record allEvent : Set := mkAllEvent {
-  src : clock;
-  num : nat
-}.
+Structure EventSpace := defEventSpace {
+  
 
-Class anEventSet (event : allEvent -> Prop) := {
-  finclock : ∃ n, ∀ c m, id c ≥ n → ¬ event {| src := c; num := m |};
-  downward : ∀ e, event {| src := src e; num := S (num e) |} → event e
 }.
-
-Class anEventSpace
-  (event : clock → nat → Prop)
-  `{anEventSet event}
-  (prec : clock → nat → clock → nat → Prop) := {
-  prec_irrefl : ∀ c n, ¬ prec c n c n;
-  prec_trans :
-    ∀ c1 n1 c2 n2 c3 n3, 
-      prec c1 n1 c2 n2 → prec c2 n2 c3 n3 → prec c1 n1 c3 n3;
-  local_linearity :
-    ∀ c n1 n2, event c n1 → event c n2 → n1 < n2 ↔ prec c n1 c n2;
-  finitenes2 :
-    ∀ c n, event c n → ∃ N, ∀ m c', event c' m → prec c' m c n → m < N
-}.
-Check anEventSpace.
-
-Structure eventSpace := mkEventSpace {
-  event : clock → nat → Prop;
-  eventSet_evidence : anEventSet event; 
-  prec : clock → nat → clock → nat → Prop;
-  event_guarantees : anEventSpace event prec
-}.
-
